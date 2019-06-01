fc1583afcb49dcc333c0ebeb40ac66ab56a656fa
Hypario <fabienduterte@orange.fr>
2019-06-01 23:43:39 +0200

    menu working and can't place 2 bombs on the same tile anymore

---
+++ Assets/Scripts/game/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Camera cam;
+    private float wantedZoom;
+    private float zoomFactor = 1.5f;
+    private float zoomSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        wantedZoom = cam.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scroll;
+        scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        wantedZoom -= scroll * zoomFactor;
+        wantedZoom = Mathf.Clamp(wantedZoom, 2f, 5f);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, wantedZoom, Time.deltaTime * zoomSpeed);
+    }
+}
--- Assets/Scripts/game/PlayerController.cs
+++ Assets/Scripts/game/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Physics2D.gravity = Vector2.zero;
+        Physics2D.gravity = Vector2.zero; // no gravity
     }
 
     // Update is called once per frame
@@ -27,18 +27,40 @@ public class PlayerController : MonoBehaviour
         Vector2 position = transform.position;
         position.x += 3.0f * horizontal * Time.deltaTime; // give the speed in units per second (cause of Time.deltaTime)
         position.y += 3.0f * vertical * Time.deltaTime; // give the speed in units per second (cause of Time.deltaTime)
-        transform.position = position;
+        transform.position = position; // set the position
 
         if (Input.GetKeyDown("space"))
         {
             // Get the cell the player is standing on
             Vector3Int cell = tilemap.WorldToCell(position);
             Vector3 cellCenterPos = tilemap.GetCellCenterWorld(cell); // get the center of the cell
+            
+            // if we aren't on a bomb, create a bomb
+            if (!BombHere(cellCenterPos))
+            {
+                // create a bomb
+                GameObject bomb = Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
 
-            // create a bomb
-            GameObject bomb = Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
-            // set the range of the bomb
-            bomb.GetComponent<BombController>().SetRange(bombRange);
+                // set the range of the bomb
+                bomb.GetComponent<BombController>().SetRange(bombRange);
+            }
         }
+    }
+
+    bool BombHere(Vector3 cell)
+    {
+        GameObject[] bombs = GameObject.FindGameObjectsWithTag("Bomb");
+        foreach (GameObject bombTest in bombs)
+        {
+            // Iterate through all bombs and find the one we are on
+
+            // If we are on a bomb
+            if (Vector3.Distance(bombTest.transform.position, cell) == 0)
+            {
+                return true;
+            }
+        }
+        // no bomb here ? return false
+        return false;
     }
 }
